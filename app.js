09b7adc85789086c9bf99bb829c11970d1c7a6db
Aaron <aaronmeade9292@gmail.com>
2017-06-27 19:38:09 -0700

    working

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ function Image(name,path) {
 
 var images = [
   new Image('tauntaun', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/tauntaun.jpg?raw=true');
-  new Image();
-  new Image();
+  new Image('Water Can', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/water-can.jpg?raw=true');
+  new Image('Dog', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/dog-duck.jpg?raw=true');
 ]
 
 
