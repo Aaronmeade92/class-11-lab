bf178c599a481e912dbf8022d8ec61987252a995
Aaron <aaronmeade9292@gmail.com>
2017-06-29 19:46:06 -0700

    added my first onclick

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict'
 
-var firstImgEl = document.getElementsByClassName('first image')[0];
-var secondImgEl = document.getElementsByClassName('second image')[0];
-var thirdImgEl = document.getElementsByClassName('third image')[0];
+var firstImgEl = document.getElementsByClassName('first-image')[0];
+var secondImgEl = document.getElementsByClassName('second-image')[0];
+var thirdImgEl = document.getElementsByClassName('third-image')[0];
 
 function Image(name,path) {
   this.name = name;
@@ -17,7 +17,6 @@ var images = [
   new Image('Water Can', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/water-can.jpg?raw=true')
 
   new Image('Dog', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/dog-duck.jpg?raw=true')
-
 ]
 
 
@@ -28,4 +27,8 @@ var water = new Image('Water Can', 'https://github.com/codefellows/seattle-201n8
 
 var dog = new Image('Dog', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/dog-duck.jpg?raw=true')
 
-formEl.addEventListener('onclick', handleSubmit);
+Image.onclick = function(event) {
+
+
+
+}
