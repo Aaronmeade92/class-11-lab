25b359964e2e8722da241ac5f75c5ae3c8725946
Aaron <aaronmeade9292@gmail.com>
2017-06-29 19:16:52 -0700

    lets get it

--- app.js
+++ app.js
@@ -12,14 +12,20 @@ function Image(name,path) {
 }
 
 var images = [
-  new Image('tauntaun', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/tauntaun.jpg?raw=true');
-  new Image('Water Can', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/water-can.jpg?raw=true');
-  new Image('Dog', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/dog-duck.jpg?raw=true');
+  new Image('tauntaun', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/tauntaun.jpg?raw=true')
+
+  new Image('Water Can', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/water-can.jpg?raw=true')
+
+  new Image('Dog', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/dog-duck.jpg?raw=true')
+
 ]
 
 
 
 var tauntaun = new Image('tauntaun', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/tauntaun.jpg?raw=true')
+
 var water = new Image('Water Can', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/water-can.jpg?raw=true')
 
 var dog = new Image('Dog', 'https://github.com/codefellows/seattle-201n8/blob/master/class-11-av-practical-clicktracker/assets/dog-duck.jpg?raw=true')
+
+formEl.addEventListener('onclick', handleSubmit);
